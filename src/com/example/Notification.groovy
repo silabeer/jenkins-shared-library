a3db7248ca677edef678ec859dc2a081c88a63e1
Kirill Borisov <kirillborisov@Kirills-MacBook-Pro.local>
2022-09-22 15:13:32 +0300

    add new class

--- src/com/example/Notification.groovy
+++ src/com/example/Notification.groovy
@@ -6,19 +6,14 @@ def steps
     Notification(steps) {
         this.steps = steps
     }
-def sendMessage(Map config = [:]) {
+def sendMessage(env,Map config = [:]) {
     def token = config.token
-    def chatID = config.chatID
-    def message = config.message ?: 'Test message'
-
+    def chatID = config.chatID ?: '123123123'    
+    def message = steps.libraryResource 'message'
+    def bindMap = [buildID: env.BUILD_NUMBER, logs: env.BUILD_URL]
+    def telegramText = new SimpleTemplateEngine().createTemplate(message).make(bindMap)
     steps.println "Start send message to Telegram"
-    steps.sh "curl -s -X POST https://api.telegram.org/bot$token/sendMessage -d chat_id=$chatID -d text=\"$message\""
+    steps.sh "curl -s -X POST https://api.telegram.org/bot$token/sendMessage -d chat_id=$chatID -d text=\"$telegramText\""
     steps.println "End send message to Telegram"
    }
-
-def readPropFile() {
-    def scriptcontents = steps.libraryResource 'test.properties'
-    def props = readProperties text: scriptcontents
-    steps.println props
-}
 }
