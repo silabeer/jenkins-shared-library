8b4199a1eb8145f1201d849275d3f97b833d5560
Kirill Borisov <kirillborisov@Kirills-MacBook-Pro.local>
2022-09-22 14:06:00 +0300

    add new class

--- src/com/example/Notification.groovy
+++ src/com/example/Notification.groovy
@@ -1,17 +1,18 @@
 package com.example
 
 class Notification {
-    String token
-    String message
-    String chatID
-    
-    def steps
+
+def steps
     Notification(steps) {
         this.steps = steps
     }
-   def sendMessage(chatID,token,message) {
-      steps.println "Start send message to Telegram"
-      steps.sh "curl -s -X POST https://api.telegram.org/bot$token/sendMessage -d chat_id=$chatID -d text=\"$message\""
-      steps.println "End send message to Telegram"
+def sendMessage(Map config = [:]) {
+    def token = config.token
+    def chatID = config.chatID
+    def message = config.message ? 'Test message'
+
+    steps.println "Start send message to Telegram"
+    steps.sh "curl -s -X POST https://api.telegram.org/bot$token/sendMessage -d chat_id=$chatID -d text=\"$message\""
+    steps.println "End send message to Telegram"
    }
 }
